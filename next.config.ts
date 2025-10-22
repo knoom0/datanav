93d367f78e4cf4854f6c9835d0a2d506a6534724
knoom <moonk.94043@gmail.com>
2025-10-21 23:32:13 -0700

    Use next-intl 4.3.12 dependency

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,7 @@
-import path from "path";
-
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack: (config) => {
-    config.resolve ??= {};
-    config.resolve.alias ??= {};
-    config.resolve.alias["next-intl"] = path.resolve(__dirname, "lib/next-intl");
-
-    return config;
-  },
+  /* config options here */
 };
 
 export default nextConfig;
